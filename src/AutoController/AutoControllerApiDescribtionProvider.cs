24dceb43834a7d56df477b4482f4db1d0108ace8
kolesnikovav <a9104656059@yandex.ru>
2023-10-12 16:22:12 +0300

    Chore update

--- src/AutoController/AutoControllerApiDescribtionProvider.cs
+++ src/AutoController/AutoControllerApiDescribtionProvider.cs
@@ -40,9 +40,13 @@ internal sealed class AutoControllerApiDescribtionProvider : IApiDescriptionProv
         var apiDescription = new ApiDescription();
         apiDescription.HttpMethod = autocontrollerMetadata.Verb;
         apiDescription.GroupName = autocontrollerMetadata.Prefix;
-
         apiDescription.ActionDescriptor = new ActionDescriptor
         {
+            DisplayName = autocontrollerMetadata.Action,
+            
+            EndpointMetadata = new List<object>() {
+                autocontrollerMetadata.Template              
+            },
             RouteValues = new Dictionary<string, string?>
             {
                 // Swagger uses this to group endpoints together.
@@ -52,126 +56,21 @@ internal sealed class AutoControllerApiDescribtionProvider : IApiDescriptionProv
             },
             //EndpointMetadata = autocontrollerMetadata.;// routeEndpoint.Metadata..ToList()
         };
+        var routeInfo = new Microsoft.AspNetCore.Mvc.Routing.AttributeRouteInfo
+        {
+            Name = autocontrollerMetadata.Template,
+            Template = autocontrollerMetadata.Template
+        };
+        apiDescription.ActionDescriptor.AttributeRouteInfo=routeInfo;
         apiDescription.SupportedRequestFormats.Add(new ApiRequestFormat { MediaType = "application/json" });
-                // apiDescription.SupportedResponseTypes.Add(new ApiResponseType
-                // {
-                //     ApiResponseFormats = { new ApiResponseFormat { MediaType = "application/json" } },
-                //     ModelMetadata = new GrpcModelMetadata(ModelMetadataIdentity.ForType(methodDescriptor.OutputType.ClrType)),
-                //     StatusCode = 200
-                // });
-        //         apiDescription.SupportedResponseTypes.Add(new ApiResponseType
-        //         {
-        //             ApiResponseFormats = { new ApiResponseFormat { MediaType = "application/json" } },
-        //             ModelMetadata = new GrpcModelMetadata(ModelMetadataIdentity.ForType(typeof(Google.Rpc.Status))),
-        //             IsDefaultResponse = true
-        //         });
-        //         var explorerSettings = routeEndpoint.Metadata.GetMetadata<ApiExplorerSettingsAttribute>();
-        //         if (explorerSettings != null)
-        //         {
-        //             apiDescription.GroupName = explorerSettings.GroupName;
-        //         }
-
-        //         // var methodMetadata = routeEndpoint.Metadata.GetMetadata<GrpcMethodMetadata>()!;
-        //         // var httpRoutePattern = HttpRoutePattern.Parse(pattern);
-        //         // var routeParameters = ServiceDescriptorHelpers.ResolveRouteParameterDescriptors(httpRoutePattern.Variables, methodDescriptor.InputType);
-
-        //         apiDescription.RelativePath = ResolvePath(httpRoutePattern, routeParameters);
-
-        //         foreach (var routeParameter in routeParameters)
-        //         {
-        //             // var field = routeParameter.Value.DescriptorsPath.Last();
-        //             // var parameterName = ServiceDescriptorHelpers.FormatUnderscoreName(field.Name, pascalCase: true, preservePeriod: false);
-        //             // var propertyInfo = field.ContainingType.ClrType.GetProperty(parameterName);
-
-        //             // If from a property, create model as property to get its XML comments.
-        //             // var identity = propertyInfo != null
-        //             //     ? ModelMetadataIdentity.ForProperty(propertyInfo, MessageDescriptorHelpers.ResolveFieldType(field), field.ContainingType.ClrType)
-        //             //     : ModelMetadataIdentity.ForType(MessageDescriptorHelpers.ResolveFieldType(field));
-
-        //             apiDescription.ParameterDescriptions.Add(new ApiParameterDescription
-        //             {
-        //                 Name = routeParameter.Value.JsonPath,
-        //                 //ModelMetadata = new GrpcModelMetadata(identity),
-        //                 Source = BindingSource.Path,
-        //                 DefaultValue = string.Empty
-        //             });
-        //         }
-
-        //         // var bodyDescriptor = ServiceDescriptorHelpers.ResolveBodyDescriptor(httpRule.Body, methodMetadata.ServiceType, methodDescriptor);
-        //         // if (bodyDescriptor != null)
-        //         // {
-        //         //     // If from a property, create model as property to get its XML comments.
-        //         //     var identity = bodyDescriptor.PropertyInfo != null
-        //         //         ? ModelMetadataIdentity.ForProperty(bodyDescriptor.PropertyInfo, bodyDescriptor.PropertyInfo.PropertyType, bodyDescriptor.PropertyInfo.DeclaringType!)
-        //         //         : ModelMetadataIdentity.ForType(bodyDescriptor.Descriptor.ClrType);
-
-        //         //     // Or if from a parameter, create model as parameter to get its XML comments.
-        //         //     var parameterDescriptor = bodyDescriptor.ParameterInfo != null
-        //         //         ? new ControllerParameterDescriptor { ParameterInfo = bodyDescriptor.ParameterInfo }
-        //         //         : null;
-
-        //         //     apiDescription.ParameterDescriptions.Add(new ApiParameterDescription
-        //         //     {
-        //         //         Name = "Input",
-        //         //         ModelMetadata = new GrpcModelMetadata(identity),
-        //         //         Source = BindingSource.Body,
-        //         //         ParameterDescriptor = parameterDescriptor!
-        //         //     });
-        //         // }
-
-        //         // var queryParameters = ServiceDescriptorHelpers.ResolveQueryParameterDescriptors(routeParameters, methodDescriptor, bodyDescriptor?.Descriptor, bodyDescriptor?.FieldDescriptor);
-        //         // foreach (var queryDescription in queryParameters)
-        //         // {
-        //         //     var fieldType = MessageDescriptorHelpers.ResolveFieldType(queryDescription.Value);
-        //         //     if (queryDescription.Value.IsRepeated)
-        //         //     {
-        //         //         fieldType = typeof(List<>).MakeGenericType(fieldType);
-        //         //     }
-
-        //         //     apiDescription.ParameterDescriptions.Add(new ApiParameterDescription
-        //         //     {
-        //         //         Name = queryDescription.Key,
-        //         //         ModelMetadata = new GrpcModelMetadata(ModelMetadataIdentity.ForType(fieldType)),
-        //         //         Source = BindingSource.Query,
-        //         //         DefaultValue = string.Empty
-        //         //     });
-        //         // }
-
+        apiDescription.SupportedResponseTypes.Add(new ApiResponseType
+        {
+            ApiResponseFormats = { new ApiResponseFormat { MediaType = "application/json" } },
+            // ModelMetadata = new GrpcModelMetadata(ModelMetadataIdentity.ForType(methodDescriptor.OutputType.ClrType)),
+            StatusCode = 200
+        });
         return apiDescription;
     }
-
-    //     private static string ResolvePath(HttpRoutePattern httpRoutePattern, Dictionary<string, RouteParameter> routeParameters)
-    //     {
-    //         var sb = new StringBuilder();
-    //         // for (var i = 0; i < httpRoutePattern.Segments.Count; i++)
-    //         // {
-    //         //     if (sb.Length > 0)
-    //         //     {
-    //         //         sb.Append('/');
-    //         //     }
-    //         //     var routeParameter = routeParameters.SingleOrDefault(kvp => kvp.Value.RouteVariable.StartSegment == i).Value;
-    //         //     if (routeParameter != null)
-    //         //     {
-    //         //         sb.Append('{');
-    //         //         sb.Append(routeParameter.JsonPath);
-    //         //         sb.Append('}');
-
-    //         //         // Skip segments if variable is multiple segment.
-    //         //         i = routeParameter.RouteVariable.EndSegment - 1;
-    //         //     }
-    //         //     else
-    //         //     {
-    //         //         sb.Append(httpRoutePattern.Segments[i]);
-    //         //     }
-    //         // }
-    //         // if (httpRoutePattern.Verb != null)
-    //         // {
-    //         //     sb.Append(':');
-    //         //     sb.Append(httpRoutePattern.Verb);
-    //         // }
-    //         return sb.ToString();
-    //     }
-
     public void OnProvidersExecuted(ApiDescriptionProviderContext context)
     {
         // no-op
