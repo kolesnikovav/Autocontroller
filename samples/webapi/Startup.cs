dac511df110cda700037fc22d372209ab8c7af86
kolesnikovav <a9104656059@yandex.ru>
2024-10-03 14:32:06 +0300

    fix samples

--- samples/webapi/Startup.cs
+++ samples/webapi/Startup.cs
@@ -1,8 +1,4 @@
-using System;
 using System.Text.Json;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,18 +6,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Sqlite;
 using AutoController;
 
 namespace webapi;
 
-public class Startup
+public class Startup(IConfiguration configuration)
 {
-    public Startup(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
-    public IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; } = configuration;
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
