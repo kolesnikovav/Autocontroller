393e5fbd00fd35432703357de366e99419485731
kolesnikovav <a9104656059@yandex.ru>
2024-10-04 14:05:24 +0300

    rework service

--- src/AutoController/AutoControllerExtention.cs
+++ src/AutoController/AutoControllerExtention.cs
@@ -22,42 +22,10 @@ public static class AutoControllerExtention
     /// </summary>
     /// <typeparam name="T">The type of your DBContext/>.</typeparam>
     /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    [Obsolete("Use version with database type and connection string")]
     public static void AddAutoController<T>(this IServiceCollection services) where T : DbContext
     {
         services.AddSingleton(typeof(AutoRouterService<T>));
     }
-    /// <summary>
-    /// Adds AutoController as singletone service and register it in Dependency Injection
-    ///
-    /// </summary>
-    /// <typeparam name="T">The type of your DBContext/>.</typeparam>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="dbType">The type of Database</param>
-    /// <param name="connString">Connection string</param>
-    /// <param name="DbContextBeforeSaveChangesMethod">Method of DbContext to execute it before save data</param>
-    /// <param name="DbContextFactory">Custom DbContextFactory</param>
-    public static void AddAutoController<T>(this IServiceCollection services, DatabaseTypes dbType, string connString, MethodInfo? DbContextBeforeSaveChangesMethod = null, Func<T>? DbContextFactory = null) where T : DbContext
-    {
-        AutoRouterService<T>.SetStaticParams(dbType, connString, DbContextBeforeSaveChangesMethod, DbContextFactory);
-        services.AddSingleton(typeof(AutoRouterService<T>));
-    }
-    /// <summary>
-    /// Adds AutoController as singletone service and register it in Dependency Injection
-    /// with Database context options
-    /// </summary>
-    /// <typeparam name="T">The type of your DBContext/>.</typeparam>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="dbType">The type of Database</param>
-    /// <param name="connString">Connection string</param>
-    /// <param name="options">Database context options </param>
-    /// <param name="DbContextBeforeSaveChangesMethod">Method of DbContext to execute it before save data</param>
-    /// <param name="DbContextFactory">Custom DbContextFactory</param>
-    public static void AddAutoController<T>(this IServiceCollection services, DatabaseTypes dbType, string connString, DbContextOptions<T> options, MethodInfo? DbContextBeforeSaveChangesMethod = null, Func<T>? DbContextFactory = null) where T : DbContext
-    {
-        AutoRouterService<T>.SetStaticParams(dbType, connString, DbContextBeforeSaveChangesMethod, DbContextFactory, options);
-        services.AddSingleton(typeof(AutoRouterService<T>));
-    }    
     /// <summary>
     /// Retrives  AutoRouterService service for external use.
     ///
@@ -133,83 +101,10 @@ public static class AutoControllerExtention
         {
             autoRouter.AttachToLogger(logger);
         }
+        var serviceProvider = appBuilder.ApplicationServices.GetService<IServiceProvider>();
         autoRouter.GetAutoControllers(
             routePrefix,
-            databaseType,
-            connectionString,
-            interactingType,
-            authentificationPath,
-            accessDeniedPath,
-            jsonOptions,
-            DefaultGetAction,
-            DefaultGetCountAction,
-            DefaultPostAction,
-            DefaultDeleteAction,
-            DefaultFilterParameter,
-            DefaultSortParameter,
-            DefaultSortDirectionParameter,
-            DefaultPageParameter,
-            DefaultItemsPerPageParameter,
-            DefaultUpdateAction);
-        foreach (var route in autoRouter.Autoroutes)
-        {
-            AddRoute(appBuilder, route.Key, route.Value);
-        }
-    }
-    /// <summary>
-    /// Adds autocontroller for DBContext.
-    ///
-    /// </summary>
-    /// <typeparam name="T">The DBContext derived type</typeparam>
-    /// <param name="appBuilder">The instance of ApplicationBuilder</param>
-    /// <param name="routePrefix">start prefix for route</param>
-    /// <param name="useLogging">log information for route</param>
-    /// <param name="interactingType">Interacting type</param>
-    /// <param name="authentificationPath">Application authentification path</param>
-    /// <param name="accessDeniedPath">Application access denied page path</param>
-    /// <param name="jsonOptions">Sets the JsonSerializerOptions</param>
-    /// <param name="DefaultGetAction">The action path of default GET request. Default = Index</param>
-    /// <param name="DefaultGetCountAction">The action path of default Count GET request. Default = Count</param>
-    /// <param name="DefaultPostAction">The action path of default POST request. Default = Save</param>
-    /// <param name="DefaultDeleteAction">The action path of default DELETE request. Default = Delete</param>
-    /// <param name="DefaultFilterParameter">default = filter</param>
-    /// <param name="DefaultSortParameter">default = sort</param>
-    /// <param name="DefaultSortDirectionParameter">default = sortdirection</param>
-    /// <param name="DefaultPageParameter">default = page</param>
-    /// <param name="DefaultItemsPerPageParameter">default = size</param>
-    /// <param name="DefaultUpdateAction">The action path of default PUT request. Default = Update</param>
-    public static void UseAutoController<T>(
-        this IApplicationBuilder appBuilder,
-        string routePrefix,
-        bool useLogging,
-        InteractingType? interactingType,
-        string authentificationPath,
-        string accessDeniedPath,
-        JsonSerializerOptions? jsonOptions = null,
-        string DefaultGetAction = "Index",
-        string DefaultGetCountAction = "Count",
-        string DefaultPostAction = "Save",
-        string DefaultDeleteAction = "Delete",
-        string DefaultFilterParameter = "filter",
-        string DefaultSortParameter = "sort",
-        string DefaultSortDirectionParameter = "sortdirection",
-        string DefaultPageParameter = "page",
-        string DefaultItemsPerPageParameter = "size",
-        string DefaultUpdateAction = "Update"
-        ) where T : DbContext
-    {
-        if (appBuilder == null)
-        {
-            throw new ArgumentNullException(nameof(appBuilder));
-        }
-        var logger = GetOrCreateLogger(appBuilder, LogCategoryName);
-        AutoRouterService<T> autoRouter = GetAutoRouterService<T>(appBuilder);
-        if (useLogging)
-        {
-            autoRouter.AttachToLogger(logger);
-        }
-        autoRouter.GetAutoControllers(
-            routePrefix,
+            serviceProvider!,
             interactingType,
             authentificationPath,
             accessDeniedPath,
