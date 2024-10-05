adcfff5723f4f92ff25261f86f9e70642721a172
Aleksandr Kolesnikov <a9104656059@yandex.ru>
2024-10-05 20:38:17 +0300

    fix dotnet.yml (3)

--- test/AppExample/Startup.cs
+++ test/AppExample/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Sqlite;
 using AutoController;
 using DbContextExample;
 
