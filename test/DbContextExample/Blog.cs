fa1f62c36fc7f0c098ee93bd462bfebf4e2286d5
Aleksandr Kolesnikov <a9104656059@yandex.ru>
2024-10-05 20:34:00 +0300

    fix test project

--- test/DbContextExample/Blog.cs
+++ test/DbContextExample/Blog.cs
@@ -1,7 +1,6 @@
 ﻿using AutoController;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Sqlite;
 
 namespace DbContextExample;
 
